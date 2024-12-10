14a270f7f624b8b1648147656223bb8c840ad53d
NicholasJKL <matveev_andrey2003@mail.ru>
2024-12-10 14:00:31 +0300

    Added styles

--- ctapp/src/App.tsx
+++ ctapp/src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import './App.css';
+import { FC } from 'react';
+import './styles.css'
 
-function App() {
+import UnitBlock from './components/UnitBlock';
+
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='ct-grid'>
+        <div className='ct-block'>
+          <UnitBlock></UnitBlock>
+        </div>
+        <div className='ct-block'></div>
+        <div className='ct-block'></div>
+      </div>
+      <div className='ct-monitor'></div>
+    </main>
   );
 }
 
---
+++ ctapp/src/components/UnitBlock.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react';
+
+
+const UnitBlock: FC = () => {
+    return (
+        <div className='ct-item'>
+            <h1>Узел</h1>
+        </div>
+    );
+}
+
+
+export default UnitBlock;
